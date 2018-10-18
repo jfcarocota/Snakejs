46fda6dea3860e95a6d340aa2b025703d2fa1aeb
jfcarocota <jfcarocota@gmail.com>
2018-10-17 23:18:35 -0700

    commit 2
    desplazamiento del pixel principal, head.

--- snake.js
+++ snake.js
@@ -3,10 +3,13 @@ const context = canvas.getContext('2d');
 
 const SIZE = 20;
 
-const square = {
+const head = {
 	x : 0,
 	y : 0
 };
+
+let dx = 0;
+let dy = 0;
 
 setInterval(main, 1000);
 
@@ -16,30 +19,41 @@ function main(){
 }
 
 function update(){
-	
+	head.x += dx;
+	head.y += dy;
 }
 
 function draw(){
-	drawObject(square);
-	square.x += SIZE;
+	context.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
+	drawObject(head);
 }
 
 function drawObject(obj){
-	context.fillRect(square.x, square.y, SIZE, SIZE);
+	context.fillRect(obj.x, obj.y, SIZE, SIZE);
 }
+
+document.addEventListener('keydown', moveSnake);
 
 function moveSnake(event){
 	switch(event.key){
 		case 'ArrowUp':
+			dx = 0;
+			dy = -SIZE;
 			console.log('move up');
 		break;
 		case 'ArrowDown':
+			dx = 0;
+			dy = SIZE;
 			console.log('move down');
 		break;
 		case 'ArrowLeft':
+			dx = -SIZE;
+			dy = 0;
 			console.log('move left');
 		break;
 		case 'ArrowRight':
+			dx = SIZE;
+			dy = 0;
 			console.log('move right');
 		break;
 	}
